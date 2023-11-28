41d442fc86a8fea3a14e2cb63f96a56a9329adeb
Thien Ho <t.ho1@my.metroed.net>
2023-11-27 21:57:31 -0800

    add

--- main.js
+++ main.js
@@ -13,26 +13,21 @@ function varLength(){
 varLength(word);
 console.log(length);
 
+
 //create slice() function
 let fruits = "homework";
-
-//create a empty array to contain the string after slicing
-let slicedFruits = [];
+//create a empty string to contain the string after slicing
+let slicedFruits = "";
 function slice(start, end){
-    //create a for loop to check through the string
-    for(let i = 0; i < fruits.length; i++){
-
-        //set fruits[start] equals to the index you start slicing
-        fruits[start] = fruits[i];
-        slicedFruits.push(fruits[start]);
-
-        //set fruits[end] equals to the index you want to stop slicing
-        fruits[end] = fruits[i];
-        slicedFruits.push(fruits[end]);
+    //create a for loop to put the letters into new string
+    for(let i = start; i < end; i++){
+        slicedFruits += fruits[i];
     }
     return slicedFruits;
 }
-console.log(slice(0, 3));
+console.log(slice(3, 8));
+
+
 
 
 
